ff5091b704b35b2fbfb0842125f00d2822c2ade3
MINDHELL <Tgpurpose640@gmail.com>
2025-06-07 19:59:34 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ CHANNEL_ID = int(os.getenv("CHANNEL_ID", "-1002661126109"))
 OWNER_ID = int(os.getenv("OWNER_ID", "7246766108"))
 WELCOME_IMAGE = os.getenv("WELCOME_IMAGE", "https://envs.sh/n9o.jpg")
 AUTO_DELETE_TIME = int(os.getenv("AUTO_DELETE_TIME", "7200"))
-DEFAULT_POINTS = int(os.getenv("DEFAULT_POINTS", "5"))
+DEFAULT_POINTS = int(os.getenv("DEFAULT_POINTS", "10"))
 DEFAULT_RESET_TIME = int(os.getenv("DEFAULT_RESET_TIME", "18000"))
 
 # ✅ Force Subscribe Setup
